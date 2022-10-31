e31757e1944d80a7441674b25580038cd6a2f68e
HyanFerraz <99298589+HyanFerraz@users.noreply.github.com>
2022-10-31 11:15:05 -0300

    Forca precisa de biblioteca de palavras

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -10,18 +10,38 @@ def jogar():
     acertou = False
 
     palavra_secreta = "lubellion"
+    letras_acertadas = []
+    chances = 6
+
+    for letra in palavra_secreta:
+        letras_acertadas.append('_')
 
     while not enforcado and not acertou:
 
+        for index in range(len(letras_acertadas)):
+            print(letras_acertadas[index], end='')
+
         chute = input('\nDigite uma letra ')
+        index = 0
 
-        index = 1
-        for letra in palavra_secreta:
-            if chute == letra:
-                print(f'A letra {chute} foi encontrada na(s) posicoes {index}')
-            index += 1
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                if chute.lower() == letra.lower():
+                    letras_acertadas[index] = chute
+                index += 1
+        elif chute not in palavra_secreta:
+            print(f'A palavra secreta nao tem a letra {chute}')
+            chances -= 1
+            print(f'Chances restantes {chances}')
 
-    print('Fim de Jogo')
+        if chances == 0:
+            enforcado = True
+
+        if '_' not in letras_acertadas:
+            acertou = True
+
+    print('\nA palavra secreta e', palavra_secreta)
+    print('\nFim de Jogo\n')
     jogos.menu()
 
 
